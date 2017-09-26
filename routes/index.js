b84516899bc80bdb5fbb4981778ff42e299a95dc
Troy Kayne <troy@Troys-MacBook-Pro.local>
2017-09-25 22:33:00 -0600

    Shitty bootstrap changes. Styling is still crap.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'The Avenue' });
 });
 
+router.get('/podcasts', function(req, res, next) {
+  res.render('PodcastsView', { title: 'The Avenue' });
+});
+
+router.get('/pop-culture', function(req, res, next) {
+  res.render('PopCultureView', { title: 'The Avenue' });
+});
+
+router.get('/music', function(req, res, next) {
+  res.render('MusicView', { title: 'The Avenue' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('AboutView', { title: 'The Avenue' });
+});
+
 module.exports = router;
